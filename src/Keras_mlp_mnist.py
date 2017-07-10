08b6e0aabfd259c89bbfb0bd56c8d732eb850b4e
Yao <chenyao123fish@163.com>
2017-07-11 01:04:23 +0800

    Build MLP and CNN model base on keras eg
    Build MLP and CNN model base on keras eg

---
+++ src/Keras_mlp_mnist.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+
+
+batch_size = 1024
+num_classes = 10
+epochs = 2
+
+#the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+x_train = x_train.reshape(-1, 784)
+x_test = x_test.reshape(-1, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /=255
+x_test /=255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+#convert class vectors to binary class matrices 
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation='softmax'))
+
+model.summary()
+config = model.get_config()
+
+model.compile(optimizer=RMSprop(),
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, 
+                    batch_size=batch_size, 
+                    epochs=epochs, 
+                    verbose=2, 
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
